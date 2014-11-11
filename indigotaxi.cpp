e32fbcb19e6e13489bc0741bccd3ef08175b7b00
Yury Luneff <koolkhel@mail.ru>
2014-11-11 16:00:09 +0300

    версия 027

--- indigotaxi.cpp
+++ indigotaxi.cpp
@@ -12,7 +12,7 @@
 #include "voicelady.h"
 
 /* main version string! */
-static const char *version = "0.1.026";
+static const char *version = "0.1.027";
 int const IndigoTaxi::EXIT_CODE_REBOOT = -123456789;
 
 IndigoTaxi::IndigoTaxi(QWidget *parent, Qt::WFlags flags)
